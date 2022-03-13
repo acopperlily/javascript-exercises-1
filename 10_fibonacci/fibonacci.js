073b3ce7738b1d6096ab234c13c661330f92b079
acopperlily <acopperlily@yahoo.com>
2022-03-13 11:11:11 -0400

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) return 'OOPS';
+  if (n < 3) return 1;
+  return (fibonacci(n - 2) + fibonacci(n - 1));
 };
+
+const nums = [1, 2, 3, 4, 6, 10, 15, 25, -25, '1', '2'];
+
+nums.forEach(num => console.log(fibonacci(num)));
 
 // Do not edit below this line
 module.exports = fibonacci;
