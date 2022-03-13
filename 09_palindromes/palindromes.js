e18298dcb0596e2c55b41989707ff2dad394551b
acopperlily <acopperlily@yahoo.com>
2022-03-13 09:44:26 -0400

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  // let newText = '';
+  // let backward = '';
+  // text = text.toLowerCase();
+  // for (let i = 0; i < text.length; i++) {
+  //   if (text[i] >= 'a' && text[i] <= 'z') {
+  //     newText += text[i];
+  //   }
+  // }
+  // for (let i = newText.length - 1; i >= 0; i--) {
+  //   backward += newText[i];
+  // }
+  // return newText == backward;
+  let modString = text.toLowerCase().split('');
+  let compare = modString.filter(letter => letter >= 'a' && letter <= 'z');
+  return compare.join('') == compare.reverse().join('');
 };
+
+const strings = [
+  'racecar',
+  'racecar!',
+  'Racecar!',
+  'A car, a man, a maraca.',
+  'Animal loots foliated detail of stool lamina.',
+  'ZZZZ car, a man, a maracaz.',
+  'no FUCKING way'
+];
+
+strings.forEach(item => console.log(palindromes(item)));
 
 // Do not edit below this line
 module.exports = palindromes;
