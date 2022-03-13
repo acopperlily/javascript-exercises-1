dcfd3f9064be54fa30031c26714e0a1c8f72f953
acopperlily <acopperlily@yahoo.com>
2022-03-13 08:56:31 -0400

    Solve calculator
    Testing git commit message here to see if this
    opens vscode. And it does, so that's pretty cool.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  // let total = 0;
+  // arr.forEach(num => total += num);
+  // return total;
+  return arr.reduce((num1, num2) => num1 + num2, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((num1, num2) => num1 * num2, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  let result = base;
+  for (let i = 1; i < exp; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 2) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
+
+console.log(sum([1, 3, 5, 7, 9]));
+
+const nums = [0, 1, 2, 5, 10];
+nums.forEach(n => console.log(factorial(n)));
 
 // Do not edit below this line
 module.exports = {
